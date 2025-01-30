10c37143710db574ecd39786ef1e8a0d88b926dc
annibal <>
2025-01-29 19:57:40 -0600

    Gun Animation & UI

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -1,5 +1,7 @@
+using System;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Gun : MonoBehaviour
 {
@@ -9,9 +11,58 @@ public class Gun : MonoBehaviour
     [SerializeField]
     private Transform _bulletPivot;
         
+    [SerializeField]
+    private Animator _weaponAnimator;
+
+    [SerializeField]
+    private int _maxBulletsNumber = 50;
+
+    [SerializeField]
+    private int _cartridgeBulletsNumber = 10;
+    private int _totalBulletsNumber = 0;
+    private int _currentBulletsNumber = 0;
+
+    private Text _bulletText;
     public void Shoot()
     {
         GameObject.Instantiate(_bullet, _bulletPivot.position,  _bulletPivot.rotation);
+        _weaponAnimator.Play("Shoot", -1, 0f);
+        _currentBulletsNumber --;
+        UpdateBulletsText();
     }
 
+    public void PickUpWeapon()
+    {
+        _totalBulletsNumber = _maxBulletsNumber;
+        _currentBulletsNumber = _cartridgeBulletsNumber;
+        _weaponAnimator.Play("GetWeapon");
+    }
+
+    public void Reload()
+    {
+        if (_totalBulletsNumber >= _cartridgeBulletsNumber)
+        {
+            _currentBulletsNumber = _cartridgeBulletsNumber;
+        }
+
+        else if (_totalBulletsNumber > 0)
+        {
+            _currentBulletsNumber = _totalBulletsNumber;
+        }
+
+        _totalBulletsNumber -= _currentBulletsNumber;
+        UpdateBulletsText();
+
+    }
+
+    public void UpdateBulletsText()
+    {
+        if (_bulletText == null)
+        {
+            _bulletText = GameObject.Find("BulletText").GetComponent<Text>();
+        }
+
+        _bulletText.text = _currentBulletsNumber + "/" + _totalBulletsNumber;
+
+    }
 }
